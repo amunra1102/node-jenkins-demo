bdd96b9d9eca059e06be5d9532e4b4a3f3ea3b77
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-06-20 18:35:21 +0700

    test:

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const port = 3000;
 
 const app = express();
 
+app.get('/', function (req, res) {
+    res.json({ data: 'Hello world' });
+});
+
 app.listen(port, () => console.log(`Server is running on Port: ${port}`));
