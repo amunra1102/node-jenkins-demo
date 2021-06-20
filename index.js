8261ebd7fa82d774a0e3fc10b941face16be5435
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-06-20 18:10:30 +0700

    test code

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
+console.log(port2);
+app.listen(port, () => console.log(`Server is running on Port: ${port}`));
